d4d9127b3d58be933689178fac0dd3f7517ef30f
Keith Grout <keithwgrout@gmail.com>
2016-06-23 13:42:46 -0400

    adding a todo list for myself

---
+++ todo.swift
@@ -0,0 +1,17 @@
+//
+//  todo.swift
+//  VirtualTourist
+//
+//  Created by KEITH GROUT on 6/23/16.
+//  Copyright © 2016 keithwgrout. All rights reserved.
+//
+
+import Foundation
+
+// app starts in map view
+// the center of the map and zoom level should be persistent
+
+// ToDO
+// watch a few videos on mapkit
+// watch a few videos on NSUserDefaults
+// watch a few videos on creating models with CoreData
