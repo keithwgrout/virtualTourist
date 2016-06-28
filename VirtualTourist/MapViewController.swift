47cae819fa79f33e2ec79d65ba4f47c503bb4d9d
Keith Grout <keithwgrout@gmail.com>
2016-06-28 12:55:05 -0400

    persisting map coordinates

--- VirtualTourist/MapViewController.swift
+++ VirtualTourist/MapViewController.swift
@@ -7,19 +7,87 @@
 //
 
 import UIKit
+import MapKit
 
 class MapViewController: UIViewController {
+    
+    @IBOutlet weak var myMapView: MKMapView!
+    let defaults = NSUserDefaults.standardUserDefaults()
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        myMapView.delegate = self
+        
+        // the region is what we need to set
+        // nsuserDefaults can only store basic data types
+        // so we need to store the doubles and turn the into a region
+        let region = myMapView.region
+        let currSpan = region.span
+        let currCenter = region.center
+        
+        let spanLatDelta = currSpan.latitudeDelta
+        let spanLonDelta = currSpan.longitudeDelta
+        
+        let lat = currCenter.latitude
+        let lon = currCenter.longitude
+        
+        // we get the region from the map on the first launch,
+        // otherwise we create it from the userdefaults
+        
+        // we need to determine if the app has launched
+        // we can store a bool value to determine that
+        
+        if defaults.valueForKey("hasLaunched") != nil{
+            // create region from nsuserdefaults
+            print("not first launch")
+            let currentLatDelta = defaults.valueForKey("spanLatDelta") as! CLLocationDegrees
+            let currentLonDelta = defaults.valueForKey("spanLonDelta") as! CLLocationDegrees
+            let centerLat = defaults.valueForKey("latitude") as! CLLocationDegrees
+            let centerLon = defaults.valueForKey("longitude") as! CLLocationDegrees
+            
+            let span = MKCoordinateSpan(latitudeDelta: currentLatDelta ,longitudeDelta: currentLonDelta )
+            let center = CLLocationCoordinate2D(latitude: centerLat, longitude: centerLon)
+            let region = MKCoordinateRegion(center: center, span: span)
+            myMapView.setRegion(region, animated: true)
+            
+        } else {
+            print("first launch")
+            // get region from map
+            defaults.setDouble(spanLatDelta, forKey: "spanLatDelta")
+            defaults.setDouble(spanLonDelta, forKey: "spanLonDelta")
+            defaults.setDouble(lat, forKey: "latitude")
+            defaults.setDouble(lon, forKey: "longitude")
+            defaults.setValue(true, forKey: "hasLaunched")
+        }
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+}
 
+extension MapViewController: MKMapViewDelegate {
+    
+    func mapView(mapView: MKMapView, regionDidChangeAnimated animated: Bool) {
+        
+        let region = mapView.region
+        let currSpan = region.span
+        let currCenter = region.center
+        
+        let spanLatDelta = currSpan.latitudeDelta
+        let spanLonDelta = currSpan.longitudeDelta
+        
+        let lat = currCenter.latitude
+        let lon = currCenter.longitude
+        
+        // 2. save the map info to nsuserdefaults every time the map changes
+        defaults.setDouble(spanLatDelta, forKey: "spanLatDelta")
+        defaults.setDouble(spanLonDelta, forKey: "spanLonDelta")
+        defaults.setDouble(lat, forKey: "latitude")
+        defaults.setDouble(lon, forKey: "longitude")
 
+    }
 }
 
--- todo.swift
+++ todo.swift
@@ -8,10 +8,46 @@
 
 import Foundation
 
-// app starts in map view
-// the center of the map and zoom level should be persistent
+// Tapping and holding the map drops a new pin. Users can place any number of pins on the map.
+// Going to just do a review on mapkit and annotations
+
+// When a pin is tapped, the app will navigate to the Photo Album view associated with the pin.
+// Going to watch some collectionView videos, as well as a bit more about understanding how to create a model in core data for a pin, and a photo
+
+
+
+// a flickr client needs to be created.
+// a bunch of images need to be downloaded
+
+
 
 // ToDO
-// watch a few videos on mapkit
-// watch a few videos on NSUserDefaults
 // watch a few videos on creating models with CoreData
+
+// The Model
+
+/*
+ 
+ I need to have a Pin entity and a Photo entity
+ 
+ Pin:
+ 
+ Facts: 
+ 
+    1 pin can have many photos
+    A pin will need to be an annotation
+    A pin will have coordinates
+    The pin will show a photo collection when tapped
+    The pins need to be persisted in core data
+ 
+ // what are the relationships between pins and photos
+ // what properties do pins have
+ // what properties do photos have
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ */
